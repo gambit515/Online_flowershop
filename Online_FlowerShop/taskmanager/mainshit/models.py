d8760014590f86a8abab61f5c77831aa33d0e4dd
gambit515 <122382344+gambit515@users.noreply.github.com>
2023-03-24 18:09:39 +0700

    0.1

--- Online_FlowerShop/taskmanager/mainshit/models.py
+++ Online_FlowerShop/taskmanager/mainshit/models.py
@@ -6,6 +6,6 @@ class Flowers(models.Model):
     title = models.CharField('Название', max_length=50)
     price = models.IntegerField('Цена Товара')
     notes = models.TextField('Описание')
-    picture = models.ImageField('Фото',upload_to='images/')
+    picture = models.ImageField('Фото',upload_to='images//%Y/%m/%d/', blank=True)
     def __str__(self):
         return self.title
---
+++ Online_FlowerShop/taskmanager/mainshit/static/mainshit/js/1.js
@@ -0,0 +1,4 @@
+var a =5;
+var b =5;
+document.write("Результат равен = ",a+b);
+console.log("Результат равен = ",a+b);
